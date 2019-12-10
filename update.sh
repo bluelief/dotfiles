ef351e3c10603ebf166c6f731b268497661fd3ab
Bluelief <33663724+bluelief@users.noreply.github.com>
2019-12-11 08:19:15 +0900

    Fix rsync file path

--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ git pull
 # Copy config files
 cp $HOME/.gitconfig.aimergirls $HOME/.gitconfig.aimergirls.bk
 cp $HOME/.ssh/config $HOME/.ssh/config.bk
-rsync -av package/dotfiles/. $HOME/ \
+rsync -av $HOME/package/dotfiles/. $HOME/ \
 --exclude "*.sh" --exclude ".gitignore" --exclude "LICENSE" --exclude "README.md" \
 --exclude "/.git/" --exclude "/program/" --exclude "/windows/"
 cp $HOME/.gitconfig.aimergirls.bk $HOME/.gitconfig.aimergirls
