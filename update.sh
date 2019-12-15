4c1733cb7cf54d424ddd04ade6e411198c5ca362
Bluelief <33663724+bluelief@users.noreply.github.com>
2019-12-15 16:55:29 +0900

    bash_profile: Change gitconfig copy script

--- update.sh
+++ update.sh
@@ -12,12 +12,12 @@ cd $HOME/package/dotfiles
 git pull
 
 # Copy config files
-cp $HOME/.gitconfig.aimergirls $HOME/.gitconfig.aimergirls.bk
+cp $HOME/.gitconfig $HOME/.gitconfig.bk
 cp $HOME/.ssh/config $HOME/.ssh/config.bk
 rsync -av $HOME/package/dotfiles/. $HOME/ \
 --exclude "*.sh" --exclude ".gitignore" --exclude "LICENSE" --exclude "README.md" \
 --exclude "/.git/" --exclude "/program/" --exclude "/windows/"
-cp $HOME/.gitconfig.aimergirls.bk $HOME/.gitconfig.aimergirls
+cp $HOME/.gitconfig.bk $HOME/.gitconfig
 cp $HOME/.ssh/config.bk $HOME/.ssh/config
 sudo chmod 600 $HOME/.ssh/config
 echo "[+] Done. Copy config files."
