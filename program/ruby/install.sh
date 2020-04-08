113f3510a07af9b710c2eef154c2945863d94e23
Bluelief <33663724+bluelief@users.noreply.github.com>
2020-04-08 22:13:17 +0900

    rust: Fix install script

--- program/ruby/install.sh
+++ program/ruby/install.sh
@@ -5,10 +5,6 @@ rm -rf $HOME/.local/lang/rbenv
 git clone https://github.com/rbenv/rbenv.git $HOME/.local/lang/rbenv
 cd $HOME/.local/lang/rbenv && src/configure && make -C src
 
-export RBENV_ROOT="$HOME/.local/lang/rbenv"
-export PATH="$RBENV_ROOT/bin:$PATH"
-eval "$(rbenv init -)"
-
 ### As an rbenv plugin
 mkdir -p $HOME/.local/lang/rbenv/plugins
 git clone https://github.com/rbenv/ruby-build.git $HOME/.local/lang/rbenv/plugins/ruby-build
