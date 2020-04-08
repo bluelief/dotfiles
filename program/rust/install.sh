6ff187ee5458c6d8d23de10b365ab8f627c30c12
Bluelief <33663724+bluelief@users.noreply.github.com>
2020-04-08 22:08:10 +0900

    rust: Fix install env

--- program/rust/install.sh
+++ program/rust/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --default-toolchain nightly
-. $HOME/.bash_profile
+
+# setup rust env
+export CARGO_HOME="$HOME/.local/lang/cargo"
+export RUSTUP_HOME="$HOME/.local/lang/rustup"
+export PATH="$CARGO_HOME/bin:$PATH"
+
 rustup target add x86_64-unknown-linux-musl
 rustc --version
