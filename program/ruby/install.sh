de70bde4ac3a8c11577930d5e7c06f4c2423c8c1
Bluelief <33663724+bluelief@users.noreply.github.com>
2020-04-08 21:54:43 +0900

    ruby: Fix script

--- program/ruby/install.sh
+++ program/ruby/install.sh
@@ -5,7 +5,10 @@ rm -rf $HOME/.local/lang/rbenv
 git clone https://github.com/rbenv/rbenv.git $HOME/.local/lang/rbenv
 cd $HOME/.local/lang/rbenv && src/configure && make -C src
 
+export RBENV_ROOT="$HOME/.local/lang/rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+eval "$(rbenv init -)"
+
 ### As an rbenv plugin
-. $HOME/.bash_profile
 mkdir -p $HOME/.local/lang/rbenv/plugins
 git clone https://github.com/rbenv/ruby-build.git $HOME/.local/lang/rbenv/plugins/ruby-build
